7e94ec094f1f69680b90bc30bc8402dece1f6b71
Benjamin Kwashie <benjaminkwashie@gmail.com>
2023-02-19 10:55:49 -0800

    4 alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: "Print if a randomly generated
+ * number is positive, negative or zero"
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if(alpha != 'q' && alpha != 'e')
+			putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
