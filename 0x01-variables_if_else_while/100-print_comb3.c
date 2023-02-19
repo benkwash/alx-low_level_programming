412d863aa9cf4abd7dfa75fe101c872aa667aaae
Benjamin Kwashie <benjaminkwashie@gmail.com>
2023-02-19 14:31:07 -0800

    100 print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: "Print if a randomly generated
+ * number is positive, negative or zero"
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int first;
+	int second;
+
+	for (first = 0; first < 9; a++)
+	{
+		for (second = 1; second < 10; b++)
+		{
+			if (first != second && second > first)
+			{
+				putchar('0' + first);
+				putchar('0' + second);
+				if (first != 8 || second != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+
+	}
+	putchar('\n');
+
+	return (0);
+}
