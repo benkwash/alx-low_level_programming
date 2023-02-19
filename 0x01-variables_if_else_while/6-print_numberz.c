45d3bee4542384ba1f093b572afd085d49950301
Benjamin Kwashie <benjaminkwashie@gmail.com>
2023-02-19 14:03:06 -0800

    6 print numbs

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,10 +9,10 @@
 
 int main(void)
 {
-	char num;
+	int num;
 
-	for (num = '0'; num <= '9'; num++)
-		putchar(num);
+	for (num = 0; num < 10; num++)
+		putchar('0' + num);
 	putchar('\n');
 	return (0);
 }
