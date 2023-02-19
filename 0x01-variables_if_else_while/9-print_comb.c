0a2e9f0dfbe4f7f3c11b005efccef8d6912a472f
Benjamin Kwashie <benjaminkwashie@gmail.com>
2023-02-19 14:19:20 -0800

    9 print

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: "Print if a randomly generated
+ * number is positive, negative or zero"
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar('0' + num);
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
