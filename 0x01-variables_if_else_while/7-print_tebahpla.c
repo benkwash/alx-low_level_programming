31ef64c5ab82454794f383833e87d79c81bcfa1e
Benjamin Kwashie <benjaminkwashie@gmail.com>
2023-02-19 14:05:40 -0800

    7 print

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: "Print if a randomly generated
+ * number is positive, negative or zero"
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a'; alpha--)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
